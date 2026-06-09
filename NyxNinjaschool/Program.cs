78558b434efbb312710656e929ec875b1ca22985
Zyrkael <sonmatrix.dev@gmail.com>
2026-06-09 17:02:55 +0700

    feat: Add JSON configuration reading support

--- NyxNinjaschool/Program.cs
+++ NyxNinjaschool/Program.cs
@@ -1,2 +1,18 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+using Microsoft.Extensions.Configuration;
+using System;
+using System.IO;
+
+var builder = new ConfigurationBuilder()
+    .SetBasePath(Directory.GetCurrentDirectory())
+    .AddJsonFile("appsettings.json", optional: false, reloadOnChange: true);
+
+IConfiguration config = builder.Build();
+
+int port = config.GetValue<int>("ServerConfig:Port");
+int maxPlayers = config.GetValue<int>("ServerConfig:MaxPlayers");
+
+Console.WriteLine("========================");
+Console.WriteLine("Server Configuration");
+Console.WriteLine($"Port: {port}");
+Console.WriteLine($"Max Players: {maxPlayers}");
+Console.WriteLine("========================");
